d3277a7eb03561929e5b83be77bab57968a445ae
Dominic Tubach <tubach@systopia.de>
2025-02-04 12:41:27 +0100

    Allow to accept/reject multiple drawdowns at once

---
+++ ang/afsearchDrawdowns.aff.php
@@ -0,0 +1,18 @@
+<?php
+use CRM_Funding_ExtensionUtil as E;
+
+return [
+  'type' => 'search',
+  'requires' => ['crmFunding'],
+  'title' => E::ts('Drawdowns'),
+  'icon' => 'fa-list-alt',
+  'server_route' => 'civicrm/funding/drawdown/list',
+  'permission' => [
+    'access Funding',
+    'administer Funding',
+  ],
+  'permission_operator' => 'OR',
+  'search_displays' => [
+    'FundingDrawdownsAll.Table',
+  ],
+];
--- managed/SavedSearch_FundingDrawdownsAll.mgd.php
+++ managed/SavedSearch_FundingDrawdownsAll.mgd.php
@@ -3,35 +3,50 @@ use CRM_Funding_ExtensionUtil as E;
 
 return [
   [
-    'name' => 'SavedSearch_funding_drawdowns',
+    'name' => 'SavedSearch_FundingDrawdownsAll',
     'entity' => 'SavedSearch',
     'cleanup' => 'always',
     'update' => 'unmodified',
     'params' => [
       'version' => 4,
       'values' => [
-        'name' => 'funding_drawdowns',
-        'label' => E::ts('Drawdowns (in Funding Case)'),
+        'name' => 'FundingDrawdownsAll',
+        'label' => E::ts('Drawdowns'),
         'api_entity' => 'FundingDrawdown',
         'api_params' => [
           'version' => 4,
           'select' => [
             'id',
-            'payout_process_id',
-            'creation_date',
             'amount',
-            'requester_contact_id.display_name',
             'status:label',
             'acception_date',
-            'reviewer_contact_id.display_name',
+            'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.identifier',
+            'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.recipient_contact_id.display_name',
             'CAN_review',
           ],
-          'orderBy' => [
-            'id' => 'DESC',
-          ],
+          'orderBy' => [],
           'where' => [],
           'groupBy' => [],
-          'join' => [],
+          'join' => [
+            [
+              'FundingPayoutProcess AS FundingDrawdown_FundingPayoutProcess_payout_process_id_01',
+              'INNER',
+              [
+                'payout_process_id',
+                '=',
+                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01.id',
+              ],
+            ],
+            [
+              'FundingCase AS FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01',
+              'INNER',
+              [
+                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01.funding_case_id',
+                '=',
+                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.id',
+              ],
+            ],
+          ],
           'having' => [],
         ],
       ],
@@ -41,32 +56,36 @@ return [
     ],
   ],
   [
-    'name' => 'SearchDisplay_funding_drawdowns.table',
+    'name' => 'SavedSearch_FundingDrawdownsAll_SearchDisplay_Table',
     'entity' => 'SearchDisplay',
     'cleanup' => 'always',
     'update' => 'unmodified',
     'params' => [
       'version' => 4,
       'values' => [
-        'name' => 'table',
+        'name' => 'Table',
         'label' => E::ts('Table'),
-        'saved_search_id.name' => 'funding_drawdowns',
+        'saved_search_id.name' => 'FundingDrawdownsAll',
         'type' => 'table',
         'settings' => [
           'description' => NULL,
           'sort' => [],
           'limit' => 10,
-          'pager' => [
-            'show_count' => FALSE,
-            'expose_limit' => TRUE,
-          ],
+          'pager' => [],
           'placeholder' => 5,
           'columns' => [
             [
               'type' => 'field',
-              'key' => 'creation_date',
-              'dataType' => 'Timestamp',
-              'label' => E::ts('Creation Date'),
+              'key' => 'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.identifier',
+              'dataType' => 'String',
+              'label' => E::ts('Identifier'),
+              'sortable' => TRUE,
+            ],
+            [
+              'type' => 'field',
+              'key' => 'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.recipient_contact_id.display_name',
+              'dataType' => 'String',
+              'label' => E::ts('Recipient'),
               'sortable' => TRUE,
             ],
             [
@@ -74,13 +93,6 @@ return [
               'key' => 'amount',
               'dataType' => 'Money',
               'label' => E::ts('Amount'),
-              'sortable' => TRUE,
-            ],
-            [
-              'type' => 'field',
-              'key' => 'requester_contact_id.display_name',
-              'dataType' => 'String',
-              'label' => E::ts('Requester'),
               'sortable' => TRUE,
             ],
             [
@@ -98,13 +110,6 @@ return [
               'sortable' => TRUE,
             ],
             [
-              'type' => 'field',
-              'key' => 'reviewer_contact_id.display_name',
-              'dataType' => 'String',
-              'label' => E::ts('Reviewer'),
-              'sortable' => TRUE,
-            ],
-            [
               'size' => 'btn-xs',
               'links' => [
                 [
@@ -117,14 +122,14 @@ return [
                     '=',
                     TRUE,
                   ],
+                  'task' => '',
                   'entity' => '',
                   'action' => '',
                   'join' => '',
                   'target' => '',
-                  'task' => '',
                 ],
                 [
-                  'path' => 'civicrm/funding/drawdown/reject?drawdownId=[id]',
+                  'path' => 'civicrm/funding/drawdown/reject?drawdownId=[id]civicrm/',
                   'icon' => 'fa-thumbs-down',
                   'text' => E::ts('Reject'),
                   'style' => 'danger',
@@ -133,14 +138,14 @@ return [
                     '=',
                     TRUE,
                   ],
+                  'task' => '',
                   'entity' => '',
                   'action' => '',
                   'join' => '',
                   'target' => '',
-                  'task' => '',
                 ],
                 [
-                  'path' => 'civicrm/funding/drawdown-document/download?drawdownId=[id]',
+                  'path' => 'civicrm/funding/drawdown-document/download?drawdownId=[id]civicrm/',
                   'icon' => 'fa-external-link',
                   'text' => E::ts('Download Document'),
                   'style' => 'default',
@@ -148,11 +153,11 @@ return [
                     'acception_date',
                     'IS NOT EMPTY',
                   ],
+                  'task' => '',
                   'entity' => '',
                   'action' => '',
                   'join' => '',
                   'target' => '_blank',
-                  'task' => '',
                 ],
               ],
               'type' => 'buttons',
